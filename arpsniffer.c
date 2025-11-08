586f02f327263fa4073a23569cc9c84192782fe9
Vignevig <vignesh93443@gmail.com>
2025-11-08 18:42:37 +0530

    code updated

--- arpsniffer.c
+++ arpsniffer.c
@@ -5,10 +5,22 @@
 #include <pcap/pcap.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
 int available_interface();
 
-int available_interface(){
+void version();
 
+void version(){
+printf("    ___    ____  ____  _____ _   ____________________________ \n");
+printf("   ╱   │  ╱ __ ╲╱ __ ╲╱ ___╱╱ │ ╱ ╱  _╱ ____╱ ____╱ ____╱ __ ╲\n");
+printf("  ╱ ╱│ │ ╱ ╱_╱ ╱ ╱_╱ ╱╲__ ╲╱  │╱ ╱╱ ╱╱ ╱_  ╱ ╱_  ╱ __╱ ╱ ╱_╱ ╱\n");
+printf(" ╱ ___ │╱ _, _╱ ____╱___╱ ╱ ╱│  ╱╱ ╱╱ __╱ ╱ __╱ ╱ ╱___╱ _, _╱ \n");
+printf("╱_╱  │_╱_╱ │_╱_╱    ╱____╱_╱ │_╱___╱_╱   ╱_╱   ╱_____╱_╱ │_│ \n");
+printf("\t\t\t\t\t\t\t\tv0.1.0\n");
+    exit(1);
+}
+
+int available_interface(){
     char error[PCAP_ERRBUF_SIZE];
     pcap_if_t *interfaces, *temp;
     int i=0;
@@ -26,11 +38,15 @@ int available_interface(){
    
 }
 
-int main(){
-    int option;
-    scanf("%d",&option);
-    printf("[*] show available interface - 1 \n");
-    printf("[*] show available interface - 1 \n");
-    available_interface();
+int help(){
+    printf()
+}
+
+
+
+
+int main(int argc , char *argv[]){
+
+
 }
 
